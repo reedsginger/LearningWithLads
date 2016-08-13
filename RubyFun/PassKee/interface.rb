e46a9e35a2b1830f46ca92fd7580c084cdd0331e
feckhardt <feckhardt15@pembrokehill.org>
2016-08-12 20:26:33 -0400

    store first time user pass and username then after that check input against the preset master

--- RubyFun/PassKee/interface.rb
+++ RubyFun/PassKee/interface.rb
@@ -1,11 +1,33 @@
-puts 'Please enter a username!'
-username = gets.chomp
-puts 'Please enter a password!'
-password = gets.chomp
+def auth
+  if File.zero?('data/master.txt')
+    puts 'Please enter a username!'
+    username = gets.chomp
+    puts 'Please enter a password!'
+    password = gets.chomp
 
-puts "The username you have entered is #{username}. The password you have entered is #{password}."
+    puts "The username you have entered is #{username}. The password you have entered is #{password}."
 
-master = open('data/master.txt', 'w')
-creds = "#{username}:#{password}"
-master.write(creds)
-master.close
+    master = open('data/master.txt', 'w')
+    creds = "#{username}:#{password}"
+    master.write(creds)
+    master.close
+    return true
+  end
+
+  puts 'Please enter your master username!'
+  username = gets.chomp
+  puts 'Please enter your master password!'
+  password = gets.chomp
+
+  data = open('data/master.txt', 'r')
+  master_user, master_pass = data.read.split(':')
+  master_user == username && master_pass == password
+end
+
+def app
+  if auth
+    puts 'Welcome!'
+  end
+end
+
+app
